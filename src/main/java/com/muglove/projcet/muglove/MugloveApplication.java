c782004cf44e793250dd3885f573891fc8a35084
cocoball <cocoball102@gmail.com>
2021-01-07 22:27:01 +0900

    Initial commit

---
+++ src/main/java/com/muglove/projcet/muglove/MugloveApplication.java
@@ -0,0 +1,13 @@
+package com.muglove.projcet.muglove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugloveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MugloveApplication.class, args);
+    }
+
+}
