c1c747afb62a4785d528727d3ac0a186a759f4ee
cocoball <cocoball102@gmail.com>
2021-01-14 23:10:51 +0900

    게시판 css추가

--- src/main/java/com/muglove/projcet/muglove/controller/TestController.java
+++ src/main/java/com/muglove/projcet/muglove/controller/TestController.java
@@ -1,16 +1,9 @@
 package com.muglove.projcet.muglove.controller;
 
-import com.muglove.projcet.muglove.dto.TestDto;
 import com.muglove.projcet.muglove.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/main")
@@ -20,6 +13,6 @@ public class TestController {
 
     @RequestMapping("/board")
     public String getBoard() {
-        return "board/board";
+        return "board/list";
     }
 }
