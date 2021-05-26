9d859d6d5641e845d393d62fa84b269e8b363705
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-05-26 22:06:08 +0900

    .

--- src/main/java/com/muglove/projcet/muglove/board/controller/entity/BoardEntity.java
+++ src/main/java/com/muglove/projcet/muglove/board/controller/entity/BoardEntity.java
@@ -21,14 +21,19 @@ public class BoardEntity extends TimeEntity {
     private String content;
     private String REG_USER_ID; // 등록자 ID
     private String REG_DTTM; // 등록일자+시간
+    private String ALT_USER_ID; // 수정자 ID
+    private String ALT_DTTM; // 수정일자+시간
 
     @Builder
-    public BoardEntity(int BRD_NO, int BRD_MNG_NO, String title, String content, String REG_USER_ID, String REG_DTTM) {
+    public BoardEntity(int BRD_NO, int BRD_MNG_NO, String title, String content, String REG_USER_ID, String REG_DTTM,
+                       String ALT_USER_ID, String ALT_DTTM) {
         this.BRD_NO = BRD_NO;
         this.BRD_MNG_NO = BRD_MNG_NO;
         this.title = title;
         this.content = content;
         this.REG_USER_ID = REG_USER_ID;
         this.REG_DTTM = REG_DTTM;
+        this.ALT_USER_ID=ALT_USER_ID;
+        this.ALT_DTTM=ALT_DTTM;
     }
 }
